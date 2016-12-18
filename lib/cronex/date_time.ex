6f5e7e33030976eb6a5ffdd2b73d917ee7428ecb
Joao Bernardo <jb.amaro95@gmail.com>
2016-12-18 20:48:39 +0000

    Fixes Cronex.DateTime documentation bugs

--- lib/cronex/date_time.ex
+++ lib/cronex/date_time.ex
@@ -16,11 +16,9 @@ defmodule Cronex.DateTime do
   @doc"""
   Returns current date and time as a `%Cronex.DateTime{}`.
 
-  When `Mix.env` is equal to `:test` it return a `%Cronex.DateTime{}` with all values equal to `0`;
-
   ## Example
   
-      iex> current_date_time = Cronex.DateTime.current_date_time
+      iex> current_date_time = Cronex.DateTime.current
       iex> current_date_time.hour > 0
       true
   """
